ac1d210ae7306cea29aeb548ba89dc76be83c30d
shahn7CVTC <35782429+shahn7CVTC@users.noreply.github.com>
2018-02-14 16:42:59 -0600

    Removed type

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase R  
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase  
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
