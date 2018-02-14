7ad83c53cc745b5d6371aabd91617e7716db6d18
Skyler Hahn <shahn7@student.cvtc.edu>
2018-02-14 22:21:59 +0000

    Made Game Home page root

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase R  
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
