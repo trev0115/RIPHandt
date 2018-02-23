72bfec3500ad51acdc6a77234e13e700410ee02e
Xeng Xiong <waterfish_21@yahoo.com>
2018-02-23 01:48:24 +0000

    google button back to off, will try again another time

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, '535553513473-k6d1467pdnmfgan2f04iiji173uvohdt.apps.googleusercontent.com', '-ysmA7l3z8mIrCXphfxY72-F', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :google_oauth2, '389169486623-ojpj6k8rpteohf7vk7o76k0nvmh9c1es.apps.googleusercontent.com', '_XCl88yuWOG1YwePqR0DDzHg', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
