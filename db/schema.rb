d3d56ba08bdfabf8ff521c825d48962f9d321cbf
Benjamin Miles <dootbin@gmail.com>
2018-02-28 17:08:22 -0600

    database fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180228221343) do
+ActiveRecord::Schema.define(version: 20180228223526) do
+
+  create_table "scores", force: :cascade do |t|
+    t.string   "user_id"
+    t.string   "score"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "profile_image"
