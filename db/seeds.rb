87db2a2f0c3c69695b476a6db68c302af0a39cfb
Benjamin Miles <dootbin@gmail.com>
2018-02-28 17:08:36 -0600

    database fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,28 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+scores =  Score.create([
+        {user_id: 10, score: 2}, 
+        {user_id: 10, score: 11}, 
+        {user_id: 20, score: 30432}, 
+        {user_id: 30, score: 520}, 
+        {user_id: 10, score: 234}, 
+        {user_id: 40, score: 88}, 
+        {user_id: 60, score: 89}, 
+        {user_id: 10, score: 99}, 
+        {user_id: 10, score: 123}, 
+        {user_id: 10, score: 333}])
+
+users = User.create([
+        {name: 'Jill'}, 
+        {name: 'Ben'}, 
+        {name: 'Bill'}, 
+        {name: 'Joe'}, 
+        {name: 'Tomas'}, 
+        {name: 'Benjamin'}])
+
+
+
