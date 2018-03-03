ffd6e0040060d82b8b1268672cae180969ed4655
Benjamin Miles <dootbin@gmail.com>
2018-03-03 10:07:42 -0600

    added twitter connection

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, '389169486623-ojpj6k8rpteohf7vk7o76k0nvmh9c1es.apps.googleusercontent.com', '_XCl88yuWOG1YwePqR0DDzHg', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
+  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
 end
