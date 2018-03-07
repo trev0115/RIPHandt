fb7c985f90f9932b4cb06d4b5cb2680b284ccece
Benjamin Miles <dootbin@gmail.com>
2018-03-07 08:16:44 -0600

    moved logic from view to controller

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -2,11 +2,19 @@ class GameController < ApplicationController
   
   def index
     @scores = Score.all
-    @top10 = @scores.limit(10).order('score desc') 
+    @top10 = @scores.order('score desc').limit(10)
   end
 
     
   def new 
    @user = User.new
+  end
+
+  def tweet_user_highscore
+    @user_top_score = @scores.user_id.find(current_user.id).order('score desc').limit(1)
+    @tweet = Tweet.new(@user_top_score.score)
+    @tweet.user_id = current_user.id 
+    @tweet.save
+    respond_with(@tweet)
   end
 end
