cd5142fa49fa632eceb4a50121d893a9f64462b3
Benjamin Miles <dootbin@gmail.com>
2018-03-07 08:18:10 -0600

    twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,19 @@ class User < ActiveRecord::Base
       name: auth_hash.info.name,
       profile_image: auth_hash.info.image,
       token: auth_hash.credentials.token,
-        secret: auth_hash.credentials.secret
+      secret: auth_hash.credentials.secret
 
     )
     user
-  end 
+  end
+
+  def twitter
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.secrets.twitter_api_key 
+      config.consumer_secret     = Rails.application.secrets.twitter_api_secret
+      config.access_token        = token
+      config.access_token_secret = secret 
+    end  
+  end
 end
 
