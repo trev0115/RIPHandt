29dbc27bf6ccd9b36e7de8fa0227af5cb870353a
Benjamin Miles <dootbin@gmail.com>
2018-03-08 17:55:28 -0600

    added route for tweeting highscore

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RIPHandt::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-
+  get 'tweet', to: 'game#tweet_user_highscore', as: 'tweet'
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
 
