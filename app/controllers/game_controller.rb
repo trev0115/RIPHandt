093d21c46a9978992a6b268eccc439956d0d5a3f
Benjamin Miles <dootbin@gmail.com>
2018-03-09 11:58:44 -0600

    topscore tweet implementation

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -7,10 +7,15 @@ class GameController < ApplicationController
 
     
   def new 
-   @user = User.new
+    @user = User.new
   end
 
   def tweet_user_highscore
-    current_user.twitter.update(test)  
+    message  = "I am playing Astroids written by Agile group 1 my highscore is #{current_user.top_score}! You can also play along at http://agile.dootb.in"
+    current_user.twitter.update(message)  
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: 'You have tweeted your highscore' }
+      format.json { head :no_content }
+    end    
   end
 end
