d53cf52c51ae9f5c107a312ec4ab202818f5aa76
Benjamin Miles <dootbin@gmail.com>
2018-03-10 15:46:46 -0600

    fixed logic

--- app/controllers/savegame_controller.rb
+++ app/controllers/savegame_controller.rb
@@ -1,9 +1,22 @@
 class SavegameController < ActionController::Base
+  def endGame
+    user_score = [arams[ :user_score ]]
+    score = Score.create(user_id: session[:user_id], score: user_score)
+    if score.save
+      respond_to do |format|
+        format.html {redirect_to root_path}
+      end
+    end
+  end
+  
   def saveGame
     #TODO save lives and ammo 
     user_score = params[:user_score]
-    @scores = Score.create(user_id: session[:user_id], score: user_score)
-    if @scores.save
+    user_bullets = params[ :user_ammo]
+    user_lives = params[:user_lives]
+    user = User.find(session[:user_id])
+    user.update(saved_score: user_score, lives: user_lives, bullets: user_ammo )
+    if user.save
       respond_to do |format|
         format.html {redirect_to root_path}
       end
