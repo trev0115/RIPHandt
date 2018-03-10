2cd3405a407fbb275c096a00d84ec8afc94b20fb
Benjamin Miles <dootbin@gmail.com>
2018-03-10 13:11:01 -0600

    commit

--- app/controllers/savegame_controller.rb
+++ app/controllers/savegame_controller.rb
@@ -2,7 +2,12 @@ class SavegameController < ActionController::Base
   def saveGame
     #TODO save lives and ammo 
     user_score = params[:user_score]
-    @scores = Score.create(user_id: session[:user_id], score: user_score)
+    user_ammo = params[:user_ammo]
+    user_life = params[:user_life]
+    @user = User.find(session[:user_id])
+    user.bullets = user_ammo
+    user.save_score = user_score
+    user.bullets
     if @scores.save
       respond_to do |format|
         format.html {redirect_to root_path}
