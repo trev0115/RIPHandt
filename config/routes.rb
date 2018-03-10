f10e1ae2d6579c9da9b0ac249013c797150166b4
Benjamin Miles <dootbin@gmail.com>
2018-03-10 11:36:05 -0600

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RIPHandt::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  get '/saveGame' controller: 'saveGame', action: 'saveGame'
+  get '/saveGame', controller: 'savegame', action: 'saveGame'
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
 
