66618474454522dfa0212312f7ab2ff599bc8cf5
Benjamin Miles <dootbin@gmail.com>
2018-03-10 16:02:29 -0600

    fixed variable name

--- app/controllers/savegame_controller.rb
+++ app/controllers/savegame_controller.rb
@@ -15,10 +15,10 @@ class SavegameController < ActionController::Base
     user_bullets = params[ :user_ammo]
     user_lives = params[:user_lives]
     user = User.find(session[:user_id])
-    user.update(saved_score: user_score, lives: user_lives, bullets: user_ammo )
+    user.update(saved_score: user_score, lives: user_lives, bullets: user_bullets )
     if user.save
       respond_to do |format|
-        format.html {redirect_to root_path}
+        format.json {redirect_to root_path}
       end
     end
   end
