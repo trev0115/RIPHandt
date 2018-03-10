a30867c54332bdb8aae63e8cf1fd0e62cb653d08
Benjamin Miles <dootbin@gmail.com>
2018-03-10 11:14:23 -0600

    added route to save game

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RIPHandt::Application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-
+  get '/saveGame' controller: 'scores', action: 'create'
   resources :sessions, only: [:create, :destroy]
   resource :home, only: [:show]
 
