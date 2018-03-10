a395991b14e0009493c84e6b71d6e010d481f75d
Benjamin Miles <dootbin@gmail.com>
2018-03-10 11:25:55 -0600

    init

---
+++ app/controllers/saveGame_controller.rb
@@ -0,0 +1,12 @@
+class SaveGameController < ActionController::Base
+  def saveGame
+    #TODO save lives and ammo 
+    user_score = params[:user_score]
+    @scores = Score.create(user_id: session[:user_id], score: user_score)
+    if @scores.save
+      respond_to do |format|
+        format.html {redirect_to root_path}
+      end
+    end
+  end
+end
